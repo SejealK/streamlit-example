2bb2aeb9be514ff50f1222ff83a67808abe09e7a
SejealK <106196718+SejealK@users.noreply.github.com>
2022-11-23 16:22:50 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,20 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+import pyvista as pv
+import numpy as np
+from stpyvista import stpyvista
 
-"""
-# Welcome to Streamlit!
+"# 🧱 Structured grid"
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+## Create coordinate data
+x = np.arange(-10, 10, 0.25)
+y = np.arange(-10, 10, 0.25)
+x, y = np.meshgrid(x, y)
+z = np.sin(np.sqrt(x**2 + y**2))
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+## Set up plotter
+plotter = pv.Plotter(window_size=[600,600])
+surface = pv.StructuredGrid(x, y, z)
+plotter.add_mesh(surface, color='teal', show_edges=True)
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+## Pass the plotter (not the mesh) to stpyvista
+stpyvista(plotter, key="surface")
